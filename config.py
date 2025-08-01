44d24166e5afa3c57a08e5b42d88fe64101d69e9
fbot05381-tech <fbot05381@gmail.com>
2025-08-01 19:04:52 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# ✅ Bot Token and Config
+TOKEN = "8237073959:AAF62fkBOMhgh1Cf6gKsyvfpjSAoAcnaX9A"
+
+# ✅ Default GIFs/Stickers
+GOAL_GIF = "https://media.giphy.com/media/l0MYEqEzwMWFCg8rm/giphy.gif"
+YELLOW_CARD_GIF = "https://media.giphy.com/media/3o7TKMt1VVNkHV2PaE/giphy.gif"
+RED_CARD_GIF = "https://media.giphy.com/media/l2SpR1R0gP9iLKEg4/giphy.gif"
+PASS_GIF = "https://media.giphy.com/media/3o7TKP9lGq0QF1F8ZC/giphy.gif"
+KICK_GIF = "https://media.giphy.com/media/3o7TKtg9n0U6iF5pLW/giphy.gif"
